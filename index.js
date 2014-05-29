97d70dc312509aa27bf3b22c0ef166417ebbbca4
TurtleKitty <jm@iqumulus.com>
2014-05-29 20:20:51 +0000

    Got SQL templates online

--- index.js
+++ index.js
@@ -22,15 +22,18 @@ var
     util = require('util'),
     express = require('express'),
     server = express(),
+    hb = require('handlebars'),
     moment = require('moment'),
     nodeDBI = require('node-dbi'),
     dbs = { },
-    dbQueryMap = { },
+    templates = { },
     clog = console.log,
     cerr = console.error,
     fmt = util.format,
-    alphaNumericRegex = /^\w+$/,
-    uuidRegex = /^[A-Fa-f0-9]{8}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{12}$/
+    alphaNumericRegex = /^[A-Za-z0-9_\-.]+$/,
+    uuidRegex = /^[A-Fa-f0-9]{8}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{12}$/,
+    sqlCommentRegex = /--+/g,
+    queryVarRegex = /\$(\w+)/g
 ;
 
 server.use(express.bodyParser());
@@ -41,6 +44,7 @@ server.on('error', function (err) {
     console.error(err.message);
 });
 
+server.get('/', showAPI);
 server.post('/db/:db/rel/:relation', addRecord);
 
 server.get('/db/:db/rel/:relation', getRecordList);
@@ -56,12 +60,14 @@ config.databases.forEach(
 
         Object.keys(db.queries).forEach(
             function (qname) {
-                var qtext = db.queries[qname],
-                    numParams = numQuestions(qtext),
+                var qtmpl = db.queries[qname],
+                    numParams = numQuestions(qtmpl),
+                    strip$ = function (x) { return x.replace('$', ''); }, 
+                    qvars = ifNull(qtmpl.match(queryVarRegex), []).map(strip$),
                     qyarr = [ ]
                 ;
 
-                dbQueryMap[qname] = db.name;
+                templates[qname] = hb.compile( qtmpl.replace(queryVarRegex, '{{ $1 }}') );
 
                 for (var i = 1; i <= numParams; i++) {
                     qyarr[i] = '/:p' + i;
@@ -72,17 +78,55 @@ config.databases.forEach(
                 server.get(
                     routePath,
                     function (req, res) {
-                        var args = [ ];
+                        var args = [ ],
+                            qvals = { },
+                            fail = ''
+                        ;
+
+                        qvars.forEach(
+                            function (qv) {
+                                var val = req.param(qv);
+
+                                if (sqlCommentRegex.test(val)) {
+                                    fail += 'SQL comments are forbidden as inputs.';
+                                    return;
+                                }
+
+                                if (! (isUUID(val) || isAlphaNumeric(val)) ) {
+                                    return itSucks(res, 'Query parameters must be alphanumeric.');
+                                }
+
+                                if (val) {
+                                    qvals[qv] = val;
+                                }
+                                else {
+                                    fail += fmt('Parameter "%s" is required!\n', qv);
+                                }
+                            }
+                        );
+
+                        if (fail.length > 0) {
+                            return itSucks(res, fail);
+                        }
 
                         for (var i = 1; i <= numParams; i++) {
                             var val = req.param( 'p' + i );
 
+                            if (!val) {
+                                return itSucks(res, fmt('Missing parameter: %s', 'p' + i));
+                            }
+
                             if (! (isUUID(val) || isAlphaNumeric(val)) ) {
-                                return itSucks(res, 'Query parameters must be alphanumeric or UUIDs.');
+                                return itSucks(res, 'Query parameters must be alphanumeric.');
                             }
 
                             args.push(val);
                         }
+
+                        var qtext = (qvars.length > 0)
+                                  ? templates[qname](qvals)
+                                  : qtmpl
+                        ;
 
                         query(
                             dbs[db.name],
@@ -113,6 +157,14 @@ http.createServer(server).listen(config.port);
 
 // utils
 
+function ifNull (x, defaultVal) {
+    if (x === null || x === undefined) {
+        return defaultVal;
+    }
+
+    return x;
+}
+
 function DBconnect (conf) {
     var db = new nodeDBI.DBWrapper(
         conf.type,
@@ -138,7 +190,7 @@ function query (db, query, args, fn) {
 }
 
 function numQuestions (query) {
-    return query.match(/\?/g).length;
+    return ifNull(query.match(/\?/g), []).length;
 }
 
 function isAlphaNumeric (x) {
@@ -162,23 +214,44 @@ function itSucks (res, error) {
 
 // routes
 
+function showAPI (req, res) {
+    res.send(server.routes);
+}
+
 function addRecord (req, res) {
     itsSucks(res, "NIY");
 }
 
 function getRecordList (req, res) {
-    var relation = req.params.relation,
-        page = req.query.page || 1,
-        count = req.query.perpage || 20,
+    var dbname = req.params.db,
+        relation = req.params.relation,
+        page = req.query.page ? parseInt(req.query.page) : 1,
+        count = req.query.perpage ? parseInt(req.query.perpage) : 20,
         sortby = req.query.sortby || 'id'
     ;
+
+    if (! isAlphaNumeric(dbname)) {
+        return itSucks(res, 'DB value must be alphanumeric.');
+    }
+
+    if (sqlCommentRegex.test(dbname)) {
+        return itSucks(res, 'DB value can not contain SQL comments.');
+    }
 
     if (! isAlphaNumeric(relation)) {
         return itSucks(res, 'Relation value must be alphanumeric.');
     }
 
+    if (sqlCommentRegex.test(relation)) {
+        return itSucks(res, 'Relation value can not contain SQL comments.');
+    }
+
     if (! isAlphaNumeric(sortby)) {
         return itSucks(res, 'SortBy value must be alphanumeric.');
+    }
+
+    if (sqlCommentRegex.test(sortby)) {
+        return itSucks(res, 'SortBy value can not contain SQL comments.');
     }
 
     if (isNaN(page)) {
@@ -198,7 +271,7 @@ function getRecordList (req, res) {
 
     var query = util.format('select * from %s order by %s offset %d limit %d', relation, sortby, offset, limit);
 
-    db.fetchAll(
+    dbs[dbname].fetchAll(
         query,
         null,
         function (err, results) {
@@ -217,8 +290,8 @@ function getRecord (req, res) {
         return itSucks(res, 'Relation value must be alphanumeric.');
     }
 
-    if (! isUUID(id)) {
-        return itSucks(res, 'ID must be a UUID.');
+    if (! isAlphaNumeric(id)) {
+        return itSucks(res, 'ID must be a alphanumeric.');
     }
 
     var query = util.format('select * from %s where id = ?', relation);
